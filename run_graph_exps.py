644e6978bcca476091deda7f893db467f769c847
anonymous-review-lab <94355726+anonymous-review-lab@users.noreply.github.com>
2023-03-22 17:22:32 +0800

    Update run_graph_exps.py

--- run_graph_exps.py
+++ run_graph_exps.py
@@ -1,13 +1,30 @@
 from Graph_level_Models.helpers.config import args_parser
 from backdoor_graph_clf import  main as backdoor_main
+from helpers.metrics_utils import log_test_results
 import  numpy as np
-
+import json
 args = args_parser()
 rs = np.random.RandomState(args.seed)
 seeds = rs.randint(1000,size=5)
+def log_test_csv(args, file_name, method):
+    metric_list = []
+    data_set = args.dataset
+    metric_list.append(data_set)
+    model_name = args.backbone
+    metric_list.append(model_name)
+
+
+    method_name = method
+    metric_list.append(method_name)
+
+
+    log_test_results(args.model_dir, metric_list, file_name)
+
 
 def main():
     local_model_local_trigger_lists, local_model_global_trigger_lists, global_model_local_trigger_lists, global_model_global_trigger_lists = [],[],[],[]
+
+    metric_list = []
     for i in range(len(seeds)):
         args.seed = seeds[i]
         local_model_metric, global_model_metric, args = backdoor_main(args)
@@ -31,10 +48,29 @@ def main():
     std_local_model_local_trigger, std_local_model_global_trigger, std_global_model_local_trigger, std_global_model_global_trigger = np.std(np.array(local_model_local_trigger_lists)), \
                                                                                                                                                              np.std(np.array(local_model_global_trigger_lists)), \
                                                                                                                                                              np.std(np.array(global_model_local_trigger_lists)), \
-                                                                                                                                                             np.std(np.array(global_model_global_trigger_lists))
+                                                                                                                                                      np.std(np.array(global_model_global_trigger_lists))
+    with open(args.config) as f:
+        config = json.load(f)
+    header = ['dataset', 'model', 'mean_local_model_local_trigger', 'std_local_model_local_trigger']
+    paths = "./checkpoints/Graph/"
+    model_name = config['model']
+    file_name = 'data-{}_model-{}_IID-{}_num_workers-{}_num_mali-{}_epoch_backdoor-{}_frac_of_avg-{}_trigger_type-{}_trigger_position-{}_poisoning_intensity-{}'.format(args.dataset,
+                                                                                                                                                                        model_name,
+                                                                                                                                                                        args.is_iid,
+                                                                                                                                                                        args.num_workers,
+                                                                                                                                                                        args.num_mali,
+                                                                                                                                                                        args.epoch_backdoor,
+                                                                                                                                                                        args.frac_of_avg,
+                                                                                                                                                                        args.trigger_type,
+                                                                                                                                                                        args.trigger_position,
+                                                                                                                                                                        args.poisoning_intensity)
 
-
-    paths = '.data'
+    log_test_results(paths, header, file_name)
+    metric_list.append(args.dataset)
+    metric_list.append(model_name)
+    metric_list.append(mean_local_model_local_trigger)
+    metric_list.append(std_local_model_local_trigger)
+    log_test_results(paths, metric_list, file_name)
 
 
 if __name__ == '__main__':
