6d51e49e84f4f6927bcd203e70aa2a2a014c0ccc
MinamiHaruto <minamiharuto143@gmail.com>
2023-03-24 13:20:38 +0800

    wandb_logger_fix

--- run_graph_exps.py
+++ run_graph_exps.py
@@ -1,38 +1,38 @@
 from Graph_level_Models.helpers.config import args_parser
 from backdoor_graph_clf import  main as backdoor_main
 from helpers.metrics_utils import log_test_results
-import  numpy as np
+import numpy as np
 import json
 import wandb
 
 args = args_parser()
 rs = np.random.RandomState(args.seed)
-seeds = rs.randint(1000,size=5)
+seeds = rs.randint(1000, size=5)
 
 
 def main(args):
+    with open(args.config) as f:
+        config = json.load(f)
+    model_name = config['model']
+    # 'data-{}_model-{}_IID-{}_num_workers-{}_num_mali-{}_epoch_backdoor-{}_frac_of_avg-{}_trigger_type-{}_trigger_position-{}_poisoning_intensity-{}'
+    file_name = 'D-{}_M-{}_IID-{}_NW-{}_NM-{}_EB-{}_FA-{}_TPye-{}_TPo-{}_PI-{}'.format(
+        args.dataset,
+        model_name,
+        args.is_iid,
+        args.num_workers,
+        args.num_mali,
+        args.epoch_backdoor,
+        args.frac_of_avg,
+        args.trigger_type,
+        args.trigger_position,
+        args.poisoning_intensity)
 
     average_all_clean_acc_list, average_local_attack_success_rate_acc_list, average_local_clean_acc_list = [], [], []
+    results_table = []
     metric_list = []
     average_local_unchanged_acc_list = []
     for i in range(len(seeds)):
         args.seed = seeds[i]
-        with open(args.config) as f:
-            config = json.load(f)
-        model_name = config['model']
-        #'data-{}_model-{}_IID-{}_num_workers-{}_num_mali-{}_epoch_backdoor-{}_frac_of_avg-{}_trigger_type-{}_trigger_position-{}_poisoning_intensity-{}'
-        file_name = 'D-{}_M-{}_IID-{}_NW-{}_NM-{}_EB-{}_FA-{}_TPye-{}_TPo-{}_PI-{}'.format(
-            args.dataset,
-            model_name,
-            args.is_iid,
-            args.num_workers,
-            args.num_mali,
-            args.epoch_backdoor,
-            args.frac_of_avg,
-            args.trigger_type,
-            args.trigger_position,
-            args.poisoning_intensity)
-
 
         # wandb init
         logger = wandb.init(
@@ -43,24 +43,41 @@ def main(args):
             config=args,
         )
 
+        average_all_clean_acc, average_local_attack_success_rate_acc, average_local_clean_acc, average_local_unchanged_acc = backdoor_main(args, logger)
+        results_table.append([average_all_clean_acc, average_local_attack_success_rate_acc, average_local_clean_acc, average_local_unchanged_acc])
+        logger.log({"average_all_clean_acc": average_all_clean_acc,
+                    "average_local_attack_success_rate_acc": average_local_attack_success_rate_acc,
+                    "average_local_clean_acc": average_local_clean_acc,
+                    "average_local_unchanged_acc": average_local_unchanged_acc})
 
-        average_all_clean_acc, average_local_attack_success_rate_acc, average_local_clean_acc,average_local_unchanged_acc = backdoor_main(args, logger)
         average_all_clean_acc_list.append(average_all_clean_acc)
         average_local_attack_success_rate_acc_list.append(average_local_attack_success_rate_acc)
         average_local_clean_acc_list.append(average_local_clean_acc)
-
         average_local_unchanged_acc_list.append(average_local_unchanged_acc)
 
         # end the logger
         wandb.finish()
 
+    # wandb table logger init
+    columns = ["average_all_clean_acc", "average_local_attack_success_rate_acc", "average_local_clean_acc", "average_local_unchanged_acc"]
+    logger_table = wandb.Table(columns=columns, data=results_table)
+    table_logger = wandb.init(
+        entity="hkust-gz",
+        project=args.proj_name,
+        group=file_name,
+        name=f"exp_results",
+        config=args,
+    )
+    table_logger.log({"results": logger_table})
+    wandb.finish()
 
     mean_average_all_clean_acc, mean_average_local_attack_success_rate_acc, mean_average_local_clean_acc = np.mean(np.array(average_all_clean_acc_list)),\
-                                                                                                                                                                 np.mean(np.array(average_local_attack_success_rate_acc_list)),\
-                                                                                                                                                                 np.mean(np.array(average_local_clean_acc_list))
+                                                                                                           np.mean(np.array(average_local_attack_success_rate_acc_list)),\
+                                                                                                           np.mean(np.array(average_local_clean_acc_list))
+
     std_average_all_clean_acc, std_average_local_attack_success_rate_acc, std_average_local_clean_acc = np.std(np.array(average_all_clean_acc_list)),\
-                                                                                                                                                                 np.std(np.array(average_local_attack_success_rate_acc_list)),\
-                                                                                                                                                                 np.std(np.array(average_local_clean_acc_list))
+                                                                                                        np.std(np.array(average_local_attack_success_rate_acc_list)),\
+                                                                                                        np.std(np.array(average_local_clean_acc_list))
 
     mean_average_local_unchanged_acc, std_average_local_unchanged_acc = np.mean(np.array(average_local_unchanged_acc_list)), np.std(np.array(average_local_unchanged_acc_list))
 
